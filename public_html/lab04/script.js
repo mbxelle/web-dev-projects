8c7af5fc6dd472d313531cfc86c094ef2d75e887
mbxelle <mbxelle@gmail.com>
2025-10-21 19:29:51 -0400

    added insert website box

---
+++ public_html/lab04/script.js
@@ -0,0 +1,20 @@
+//logic for website: 
+//input your fav website link (save it in a variable called link)
+    //if link is not valid print "invalid link must include https or http"
+// if https -> show lock padloc beside and print link 
+// if http -> show open padlock icon and print link
+//print in bookmarks list 
+
+//addition: add option for users to delete the link 
+
+
+//---function verifies if the link starts with https or http
+
+
+
+function getlink(){
+    if (rawlink.startsWith("https://"))
+        console.log("secure website")
+
+}
+
