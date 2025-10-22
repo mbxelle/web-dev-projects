c3a6f99557bf0b6bd5436c22fe32e08b578a6c66
mbxelle <mbxelle@gmail.com>
2025-10-21 21:14:49 -0400

    finished: added link identifier and temporary bookmark list feature

--- public_html/lab04/script.js
+++ public_html/lab04/script.js
@@ -10,24 +10,39 @@
 
 //---function verifies if the link starts with https or http
 
+//booklist is the array holding all the fav websites
+const bookmark = [];
+
 
 //input rawlink -> get out formatted link (icon +link)
-    function getlink(rawlink) {
-        //this gets the value of the input when user presses button
-        const input = document.getElementById("rawlink").value;
-        //if the link starts with https
-        if (input.startsWith("https://")) {
-            alert("secure website");//for testing
-            return "🔒 " + input; //secure log in
-        } else if (input.startsWith("http://")) {
-            alert("not secure website"); //for testing 
-            return "🔓 " + input; //not secure login
-        } else {
-            alert("this link does not include https or http. try again.");
-            return null;
-        }
+function getlink() {
+    //this gets the value of the input when user presses button
+    const input = document.getElementById("rawlink").value;
+    //if the link starts with https
+    if (input.startsWith("https://")) {
+        alert("secure website");//for testing   
+        return "🔒 " + input; //secure log in
+    } else if (input.startsWith("http://")) {
+        alert("not secure website"); //for testing 
+        return "🔓 " + input; //not secure login
+    } else {
+        alert("this link does not include https or http. try again.");
+        return null;
     }
+}
 
+//if it is not null or empty, add to the bookmark-list 
+function addFav() {
+  const iconlink = getlink();
+  if (iconlink !== null) {
+    bookmark.push(iconlink);
+    showBookmarks();
+  }
+}
+
+function showBookmarks() {
+  document.getElementById("bookmark-list").innerHTML = bookmark.join("<br>");
+}
 
 
 
