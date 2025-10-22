6bec143759242eaaec577feedd6dfd835d0d18d8
mbxelle <mbxelle@gmail.com>
2025-10-21 20:11:54 -0400

    added javascript code to identify secure/non secure links

--- public_html/lab04/script.js
+++ public_html/lab04/script.js
@@ -1,9 +1,9 @@
 //logic for website: 
 //input your fav website link (save it in a variable called link)
-    //if link is not valid print "invalid link must include https or http"
+//if link is not valid print "invalid link must include https or http"
 // if https -> show lock padloc beside and print link 
 // if http -> show open padlock icon and print link
-//print in bookmarks list 
+//print both results in bookmarks list 
 
 //addition: add option for users to delete the link 
 
@@ -11,10 +11,23 @@
 //---function verifies if the link starts with https or http
 
 
+//input rawlink -> get out formatted link (icon +link)
+    function getlink(rawlink) {
+        //this gets the value of the input when user presses button
+        const input = document.getElementById("rawlink").value;
+        //if the link starts with https
+        if (input.startsWith("https://")) {
+            alert("secure website");//for testing
+            return "🔒 " + input; //secure log in
+        } else if (input.startsWith("http://")) {
+            alert("not secure website"); //for testing 
+            return "🔓 " + input; //not secure login
+        } else {
+            alert("this link does not include https or http. try again.");
+            return null;
+        }
+    }
 
-function getlink(){
-    if (rawlink.startsWith("https://"))
-        console.log("secure website")
 
-}
+
 
